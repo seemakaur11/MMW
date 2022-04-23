445e044a2b242500e17334232076cdec7113cd0a
seemakaur11 <raiseema248@gmail.com>
2022-04-23 12:55:39 -0400

    route section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import "./About.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-
 import NavbarData from "./components/Navbar";
+import About from "./components/About";
 import Footer from "./components/Footer";
-import Customer from "./components/Customer";
+
 function App() {
   return (
     <div>
-      <NavbarData />
-      <Home />
-      <Footer />
-      <Customer />
+      <BrowserRouter>
+        <NavbarData />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
